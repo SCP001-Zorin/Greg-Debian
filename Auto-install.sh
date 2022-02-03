d8bceb96f327a3e0292619bdee351386cb50c444
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-02-03 13:54:06 -0500

    Create Auto-install.sh

---
+++ Auto-install.sh
@@ -0,0 +1,12 @@
+ #! /bin/sh.
+ 
+ #install lightdm
+ sh light-dm
+ 
+ #install lishtdm greeter 
+ sudo apt-get install lightdm-gtk-greeter
+ 
+ #lightdm settings
+ sudo apt install lightdm-gtk-greeter-settings
+ 
+ #End
