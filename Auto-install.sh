2508c7edf70c2a3a5cd2948a0ed762bf3f50f8a0
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-02-03 16:40:06 -0500

    Update Auto-install.sh

--- Auto-install.sh
+++ Auto-install.sh
@@ -1,12 +1,30 @@
  #! /bin/sh.
  
+ #install snapd
+ sh snap-install.sh
+ 
  #install lightdm
- sh light-dm
+ sh lightdm-install.sh
  
  #install lishtdm greeter 
  sudo apt-get install lightdm-gtk-greeter
  
  #lightdm settings
  sudo apt install lightdm-gtk-greeter-settings
+ 
+ #Alacritty terminal Emulator 
+ sh alacritty-install.sh
+ 
+ #lightdm Aether
+ sh lightdm-aether-install.sh
+ 
+ #Rofi
+ sh rofi-install.sh
+ 
+ #Fish
+ sh fish-install.sh
+ 
+ #neofetch 
+ sudo apt-get install neofetch
  
  #End
