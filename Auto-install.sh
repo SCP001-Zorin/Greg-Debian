2eb52986e237141b5cec606b25e1efbe343399fc
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-02-04 00:37:14 -0500

    Update Auto-install.sh

--- Auto-install.sh
+++ Auto-install.sh
@@ -57,4 +57,9 @@
  #Wallpaper
  sudo mkdir $HOME .Wallpaper
  
+ #Chrome
+ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+ sudo apt install ./google-chrome-stable_current_amd64.deb -y
+
+ 
  #End
