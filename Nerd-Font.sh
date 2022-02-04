6f03b341b9143111247f2dc8aa128572ee12e64d
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-02-04 15:02:43 -0500

    Update Nerd-Font.sh

--- Nerd-Font.sh
+++ Nerd-Font.sh
@@ -1,7 +1,14 @@
 #! /bin/sh.
 
-sudo mkdir -p ~/.local/share/fonts
+sudo mkdir -p ~/.local/share/fonts/Nerd-Fonts
 
-sudo cp Nerd-Font.ttf ~/.local/share/fonts/
+sudo cp Nerd-Font.ttf ~/.local/share/fonts/Nerd-Fonts
+
+#Fonts-Alternative
+
+sudo mkdir -p /usr/share/fonts/Nerd-Fonts
+
+sudo cp Nerd-Font.ttf /usr/share/fonts/Nerd-Fonts
+
 
 sudo fc-cache -f -v 
