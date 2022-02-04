a688a6c70207c98d91101cf23e913712af412bcf
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-02-04 15:15:42 -0500

    Create cp.sh

---
+++ cp.sh
@@ -0,0 +1,13 @@
+#! /bin/sh.
+
+ #alacritty configuration
+ cp -r alacritty/ ~/.config
+
+ #neofetch configuration
+ cp -r neofetch/ ~/.config  
+ 
+ #qtile Configurations
+ cp -r qtile/ ~/.config
+
+ #Rofi
+ cp -r rofi ~/.donfig
