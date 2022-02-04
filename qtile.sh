c405937c23af28f36a26fea012cb29a668dfa87c
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-02-04 18:19:10 -0500

    Update qtile.sh

--- qtile.sh
+++ qtile.sh
@@ -6,15 +6,15 @@ sudo apt -y install python3-pip
 
 sudo apt -y install python-gobject python-dbus libpangocairo-1.0-0
 
-sudo pip install xcffib
+pip install xcffib
 
-sudo pip install --no-cache-dir cairocffi
+pip install --no-cache-dir cairocffi
 
 git clone git://github.com/qtile/qtile.git
 
 cd qtile
 
-sudo pip3 install .
+pip3 install .
 
 sudo cp qtile.desktop /usr/share/xsessions
 
