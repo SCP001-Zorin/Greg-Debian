c7856d47d615ea0d21f34a44bd5569b2f1e90378
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-02-05 05:24:46 -0500

    Update cp.sh

--- cp.sh
+++ cp.sh
@@ -27,6 +27,24 @@
  #Qtile XSession
  sudo cp -r qtile.desktop /usr/share/xsessions
  
+ sudo mkdir -p ~/.local/share/fonts/Nerd-Fonts
+
+ sudo cp Nerd-Font.ttf ~/.local/share/fonts/Nerd-Fonts
+
+ sudo cp Nerd-Font.ttf ~/.local/share/fonts
+
+ #Fonts-Alternative Ubuntu Nerd Complete
+
+ sudo mkdir -p /usr/share/fonts/Nerd-Fonts
+
+ sudo cp Nerd-Font.ttf /usr/share/fonts/Nerd-Fonts
+
+ sudo rm /usr/share/fonts/truetype/dejavu/DejaVuSans.ttf
+
+ sudo rm /usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf
+
+ sudo fc-cache -f -v 
+ 
  #End
  
  
