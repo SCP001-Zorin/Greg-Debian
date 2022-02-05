0995e226e3d8e092e04f47f0de80451b2d46131c
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-02-05 02:56:35 -0500

    Update tm.sh

--- tm.sh
+++ tm.sh
@@ -1,4 +1,8 @@
 #Python
+
+#qtile pip
+export PATH=$PATH:$HOME/bin
+
 pip3 install xcffib
 
 pip3 install --no-cache-dir cairocffi
@@ -8,3 +12,5 @@ git clone git://github.com/qtile/qtile.git
 cd qtile
 
 pip3 install .
+
+sudo cp -r ~/.local/bin/qtile /usr/local/bin
