ec7b751b44b9d348156fae63ec3eac3f6bf88d75
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-02-04 20:45:01 -0500

    Update Nerd-Font.sh

--- Nerd-Font.sh
+++ Nerd-Font.sh
@@ -10,5 +10,8 @@ sudo mkdir -p /usr/share/fonts/Nerd-Fonts
 
 sudo cp Nerd-Font.ttf /usr/share/fonts/Nerd-Fonts
 
+sudo rm /usr/share/fonts/truetype/dejavu/DejaVuSans.ttf
+
+sudo rm /usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf
 
 sudo fc-cache -f -v 
