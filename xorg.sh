b432b638ab8e43fad7218487b29a9cf59793cb50
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-02-05 15:53:46 -0500

    Update xorg.sh

--- xorg.sh
+++ xorg.sh
@@ -12,13 +12,6 @@ sudo apt-get install xserver-xorg
 #Xinit
 sudo apt-get install  xinit
 
-#start Xserver
-startx
-
-#Core System
-
-#sh core.sh
-
 # install snapd
 sudo apt-get install snapd
 
@@ -27,5 +20,8 @@ sudo snap install core
 
 #alacritty from snap-store
 sudo snap install alacritty --classic
+
+#start Xserver
+startx
 
 #End
