9c2cd33c8e1d97429c6d358924372769a8ff29eb
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-02-23 22:40:01 -0500

    Add files via upload

---
+++ Core/qtile.sh
@@ -0,0 +1,19 @@
+#Python
+
+#add PATH to system
+export PATH=$PATH:$HOME/.local/bin
+
+pip3 install xcffib
+
+pip3 install --no-cache-dir cairocffi
+
+#git clone git://github.com/qtile/qtile.git
+
+#cd $HOME/qtile
+
+#pip3 install .
+
+export PATH=$PATH:$HOME/.local/bin
+pip3 install qtile
+
+#End
---
+++ Core/wm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#vim
+sudo apt-get install vim 
+
+#python3
+sudo apt-get install python3
+
+#python pip
+sudo apt-get install python3-pip
+
+#python
+sudo apt-get install python3-dbus
+
+#python
+sudo apt-get install libpangocairo-1.0-0 
+
+#python
+sudo apt-get install python3-v-sim
+
+#python
+sudo apt-get  install suckless-tools
+
+#python
+sudo apt-get  istall python3-psutil
+
+#End
