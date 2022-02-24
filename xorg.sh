bb4e3fae127d3c8dde4894f3e55bd2f4237274fd
SCP-001 <97075187+SCP001-Zorin@users.noreply.github.com>
2022-02-23 23:42:37 -0500

    Add files via upload

---
+++ xorg.sh
@@ -0,0 +1,27 @@
+#Xorg X11
+
+#mkdir
+sudo mkdir ~/.local/bin
+export PATH=$PATH:$HOME/.local/bin
+
+sudo apt-get install xorg
+
+#Xorg-server
+sudo apt-get install xserver-xorg
+
+#Xinit
+sudo apt-get install  xinit
+
+# install snapd
+sudo apt-get install snapd
+
+#install snap-core
+sudo snap install core
+
+#alacritty from snap-store
+sudo snap install alacritty --classic
+
+#start Xserver
+startx
+
+#End
